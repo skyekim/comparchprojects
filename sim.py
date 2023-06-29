0880a29f2e4b2a26d886c4d321ad64456f8f8e3b
Skye Kim <85697074+skyekim@users.noreply.github.com>
2023-06-29 11:36:38 -0400

    Update sim.py

--- sim.py
+++ sim.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 """
-CS-UY 2214
-Jeff Epstein
-Starter code for E20 simulator
-sim.py
+Code for simulator that takes machine code as the input and outputs assembly code
 """
 
 from collections import namedtuple
